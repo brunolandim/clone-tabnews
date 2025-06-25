4b52758d0afbc8b246f616269fe8561bd4e64de3
Bruno Landim <84324153+brunolandim@users.noreply.github.com>
2025-06-25 20:43:49 +0000

    add  and

--- infra/database.js
+++ infra/database.js
@@ -1,20 +1,23 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const query = async (queryString) => {
-    console.log(process.env)
-    const client = new Client({
-        host:process.env.POSTGRES_HOST,
-        port:process.env.POSTGRES_PORT,
-        password:process.env.POSTGRES_PASSWORD,
-        user:process.env.POSTGRES_USER,
-        database:process.env.POSTGRES_DB
-    })
 
-    client.connect()
-    const result = await client.query(queryString)
-    await client.end()
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    password: process.env.POSTGRES_PASSWORD,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+  });
 
-    return result
-}
+  client.connect();
+  const result = await client.query(queryString);
+  await client.end();
 
-module.exports = { query:query };
+  return result;
+};
+
+module.exports = { query: query };
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,7 +2,6 @@ const database = require("../../../../../infra/database.js");
 
 test("GET to /api/v1/status shoud be 200", async () => {
   const result = await database.query("SELECT 1 +  1;")
-  console.log(result)
   const response = await fetch("http://localhost:3000/api/v1/status");
 
   expect(response.status).toBe(200);
