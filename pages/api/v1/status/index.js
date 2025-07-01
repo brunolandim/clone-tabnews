44b02a01e1e0d9f5f99ce20adf7f8caedd7faa32
Bruno Landim <84324153+brunolandim@users.noreply.github.com>
2025-07-01 18:56:51 +0000

    finaly status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,30 @@
-function status(request, response) {
+import database from "infra/database";
+
+async function status(request, response) {
+  const result = await database.query(`
+    SELECT
+      current_setting('server_version') AS server_version,
+      current_setting('max_connections')::int AS max_connections ;
+  `);
+  
+  const databaseName = process.env.POSTGRES_DB;
+  const dataBaseConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const { server_version, max_connections } = result.rows[0];
+
+  const updatedAt = new Date().toISOString();
   response.status(200).json({
-    chave: "jonathan é gay d+",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: server_version,
+        max_connections: parseInt(max_connections),
+        connections_in_use: dataBaseConnections.rows[0].count,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,8 +1,12 @@
-const database = require("../../../../../infra/database.js");
-
 test("GET to /api/v1/status shoud be 200", async () => {
-  const result = await database.query("SELECT 1 +  1;");
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
 
   expect(response.status).toBe(200);
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.dependencies.database.version).toBe("16.9");
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+
+  expect(responseBody.dependencies.database.connections_in_use).toBe(1);
 });
